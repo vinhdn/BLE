84abe06ce74e688beee13083a37a49078c3cdeb7
Vinhdn <vinhdn07@gmail.com>
2022-06-16 17:03:49 +0700

    config kết nối bộ đàm

--- samples/app-mobile/src/androidMain/kotlin/com/beepiz/blegattcoroutines/sample/MainActivity.kt
+++ samples/app-mobile/src/androidMain/kotlin/com/beepiz/blegattcoroutines/sample/MainActivity.kt
@@ -2,6 +2,9 @@ package com.beepiz.blegattcoroutines.sample
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanSettings
 import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import androidx.activity.viewModels
@@ -9,7 +12,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.beepiz.blegattcoroutines.sample.common.BleScanHeater
 import com.beepiz.blegattcoroutines.sample.common.MainViewModel
+import com.beepiz.blegattcoroutines.sample.common.bluetoothLeScanner
+import com.beepiz.blegattcoroutines.sample.common.scanFlow
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOn
 import splitties.dimensions.dip
 import splitties.views.dsl.core.add
 import splitties.views.dsl.core.button
@@ -19,6 +27,7 @@ import splitties.views.dsl.core.verticalLayout
 import splitties.views.gravityCenterHorizontal
 import splitties.views.onClick
 import splitties.views.padding
+import timber.log.Timber
 
 @SuppressLint("SetTextI18n") // This is just a sample, English is enough.
 class MainActivity : AppCompatActivity() {
@@ -35,12 +44,26 @@ class MainActivity : AppCompatActivity() {
             if (SDK_INT >= 21) launch { BleScanHeater.heatUpWhileStarted(lifecycle) }
 
             @Suppress("MissingPermission")
+            if (SDK_INT >= 21) launch {
+                val scanSettings = ScanSettings.Builder().apply {
+                    setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                }.build()
+                bluetoothLeScanner?.scanFlow(listOf(ScanFilter.Builder()
+                    .build()), scanSettings)?.collect {
+                    if (it.device.type == BluetoothDevice.DEVICE_TYPE_LE)
+                    Timber.d("${it.device.address} ${it.device.name} ${it.device.type}")
+                }
+            }
+
+            @Suppress("MissingPermission")
             contentView = verticalLayout {
                 padding = dip(16)
                 val lp = lParams(gravity = gravityCenterHorizontal)
                 add(button {
                     text = "Log name and appearance of default device"
-                    onClick { viewModel.logNameAndAppearance() }
+                    onClick {
+                        viewModel.logNameAndAppearance()
+                    }
                 }, lp)
             }
         }
--- samples/common/src/androidMain/kotlin/com/beepiz/blegattcoroutines/sample/common/MainViewModel.kt
+++ samples/common/src/androidMain/kotlin/com/beepiz/blegattcoroutines/sample/common/MainViewModel.kt
@@ -1,24 +1,29 @@
 package com.beepiz.blegattcoroutines.sample.common
 
 import android.Manifest
+import android.bluetooth.BluetoothGattCharacteristic
 import androidx.annotation.RequiresApi
 import androidx.annotation.RequiresPermission
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.beepiz.blegattcoroutines.genericaccess.GenericAccess
 import com.beepiz.blegattcoroutines.sample.common.extensions.deviceFor
+import com.beepiz.blegattcoroutines.sample.common.extensions.print
+import com.beepiz.blegattcoroutines.sample.common.extensions.printWithCharacteristics
 import com.beepiz.blegattcoroutines.sample.common.extensions.useBasic
+import com.beepiz.bluetooth.gattcoroutines.BGD
 import kotlinx.coroutines.*
 import splitties.toast.UnreliableToastApi
 import splitties.toast.toast
 import timber.log.Timber
+import java.util.*
 
 @Suppress("InlinedApi")
 class MainViewModel : ViewModel() {
 
     private val myEddystoneUrlBeaconMacAddress = "F2:D6:43:93:70:7A"
     private val iBks12MacAddress = "F6:61:CF:AF:D0:07"
-    private val iBksPlusMacAddress = "DF:A7:85:12:7D:B1"
+    private val iBksPlusMacAddress = "BA:03:4C:42:37:81"
     private val defaultDeviceMacAddress = iBksPlusMacAddress
 
     private var operationAttempt: Job? = null
@@ -28,17 +33,36 @@ class MainViewModel : ViewModel() {
     @OptIn(UnreliableToastApi::class)
     fun logNameAndAppearance(
         deviceMacAddress: String = defaultDeviceMacAddress,
-        connectionTimeoutInMillis: Long = 5000L
+        connectionTimeoutInMillis: Long = 15000L
     ) {
         operationAttempt?.cancel()
         operationAttempt = viewModelScope.launch(Dispatchers.Main) {
             deviceFor(deviceMacAddress).useBasic(connectionTimeoutInMillis) { device, services ->
                 services.forEach { Timber.d("Service found with UUID: ${it.uuid}") }
-                with(GenericAccess) {
+                services.forEach { Timber.d(it.printWithCharacteristics()) }
+                val a = with(GenericAccess) {
                     device.readAppearance()
                     Timber.d("Device appearance: ${device.appearance}")
                     device.readDeviceName()
                     Timber.d("Device name: ${device.deviceName}".also { toast(it) })
+                    device.allNotifications.collect {
+                        Timber.i(it.print())
+                    }
+                    device.readCharacteristic(BluetoothGattCharacteristic(UUID.fromString("00001800-0000-1000-8000-00805f9b34fb"), 0x01, 0).apply {
+                        value = "57480D30013430322E3437353000000000".toByteArray()
+                    }).apply {
+                        Timber.d("data ${this.value}")
+                    }
+                    device.writeCharacteristic(BluetoothGattCharacteristic(UUID.fromString("00001800-0000-1000-8000-00805f9b34fb"), 0x01, 0).apply {
+                        value = "57480D30013430322E3437353000000000".toByteArray()
+                    }).apply {
+                        Timber.d("data ${this.print()}")
+                    }
+                    device.readDescriptor(BGD(UUID.fromString("00001800-0000-1000-8000-00805f9b34fb"), 0).apply {
+                        value = "57480D30013430322E3437353000000000".toByteArray()
+                    }).apply {
+                        Timber.d("data ${this.print()}")
+                    }
                 }
             }
             operationAttempt = null
